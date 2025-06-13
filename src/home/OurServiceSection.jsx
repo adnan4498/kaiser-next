b39278b8517dcd6808a65a5baede24583982a100
adnan4498 <akhan123456008@gamil.com>
2025-06-14 02:05:08 +0500

    all animations completed for 1440p screen

--- src/home/OurServiceSection.jsx
+++ src/home/OurServiceSection.jsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import furnitureImg from "../../public/custom-furniture-img.svg";
 import rectangleImg from "../../public/rectangle-box.svg";
 import magicWandImg from "../../public/magic-wand.svg";
@@ -8,65 +8,121 @@ import { gsap } from "gsap";
 const OurServiceSection = () => {
   const wandRef = useRef();
   const boxRef = useRef();
+  const overlayRefs = useRef([]);
+  const urbanDivRefs = useRef([]);
+  const videoRefs = useRef([]);
 
   useEffect(() => {
-
-        gsap.fromTo(
+    gsap.fromTo(
       boxRef.current,
+      { y: 0, x: 0 },
       {
-        y: 0, // Start at original Y
-        x: 0, // Start at original X
-      },
-      {
-        y: 10, // Move up 30px
-        x: 10, // Move right 30px
+        y: 10,
+        x: 10,
         duration: 2.5,
         repeat: -1,
         yoyo: true,
         ease: "power1.inOut",
       }
     );
-
 
     gsap.fromTo(
       wandRef.current,
+      { y: 0, x: 0 },
       {
-        y: 0, // Start at original Y
-        x: 0, // Start at original X
-      },
-      {
-        y: -30, // Move up 30px
-        x: -30, // Move right 30px
+        y: -30,
+        x: -30,
         duration: 2.5,
         repeat: -1,
         yoyo: true,
         ease: "power1.inOut",
       }
     );
+
+    overlayRefs.current.forEach((overlay, index) => {
+      if (!overlay) return;
+      const parent = overlay.parentNode;
+
+      const onEnter = () => {
+        gsap.to(overlay, {
+          y: 0,
+          opacity: 0.6,
+          duration: 0.5,
+          ease: "power2.out",
+        });
+
+        gsap.to(urbanDivRefs.current[index], {
+          paddingLeft: "30px",
+          paddingRight: "30px",
+          duration: 0.5,
+          ease: "power2.out",
+        });
+
+        videoRefs.current[index]?.play();
+
+        gsap.to(videoRefs.current[index], {
+          scale: 1,
+          opacity: 1,
+          duration: 0.5,
+          ease: "power2.out",
+        });
+      };
+
+      const onLeave = () => {
+        gsap.to(overlay, {
+          y: "100%",
+          opacity: 0,
+          duration: 0.5,
+          ease: "power2.inOut",
+        });
+
+        gsap.to(urbanDivRefs.current[index], {
+          paddingLeft: "0px",
+          paddingRight: "0px",
+          duration: 0.5,
+          ease: "power2.inOut",
+        });
+
+        gsap.to(videoRefs.current[index], {
+          scale: 0,
+          opacity: 0,
+          duration: 0.5,
+          ease: "power2.inOut",
+        });
+      };
+
+      parent.addEventListener("mouseenter", onEnter);
+      parent.addEventListener("mouseleave", onLeave);
+
+      return () => {
+        parent.removeEventListener("mouseenter", onEnter);
+        parent.removeEventListener("mouseleave", onLeave);
+      };
+    });
   }, []);
 
-  let furnitureCards = [
+  const furnitureCards = [
     {
       id: 0,
       img: furnitureImg,
       heading: "CUSTOM FURNITURE DESIGN",
-      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.",
+      text: "Lorem ipsum dolor sit amet...",
     },
     {
       id: 1,
       img: furnitureImg,
       heading: "FULL INTERIOR DESIGN & CONSULTATION",
-      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.",
+      text: "Lorem ipsum dolor sit amet...",
     },
     {
       id: 2,
       img: furnitureImg,
       heading: "SUSTAINABLE DESIGN SOLUTIONS",
-      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.",
+      text: "Lorem ipsum dolor sit amet...",
     },
   ];
 
-  let modernUrbanData = [
+  const modernUrbanData = [
     {
       id: 0,
       btnName: "Transformation",
@@ -93,10 +149,12 @@ const OurServiceSection = () => {
     },
   ];
 
+  const [hoveredIndex, setHoveredIndex] = useState(null);
+
   return (
     <div className="2xl:pt-[202px] 2xl:relative">
       <div
-        className="2xl:absolute 2xl:right-[100px] 2xl:top-[53px] "
+        className="2xl:absolute 2xl:right-[100px] 2xl:top-[53px]"
         ref={boxRef}
       >
         <Image src={rectangleImg} width={220} height={220} />
@@ -140,36 +198,62 @@ const OurServiceSection = () => {
           ))}
         </div>
 
-        <div className="2xl:mt-[137px] 2xl:text-center ">
+        <div className="2xl:mt-[137px] 2xl:text-center">
           <div className="2xl:text-[20px] font-semibold">Our Work</div>
           <div className="2xl:flex justify-center 2xl:mt-[23px]">
-            <div className="2xl:w-[995px] 2xl:text-[64px] 2xl:leading-[64px] 2xl:tracking-tighter ">
+            <div className="2xl:w-[995px] 2xl:text-[64px] 2xl:leading-[64px] 2xl:tracking-tighter">
               EXQUISITE CUSTOM FURNITURE AND INTERIOR DESIGNS TAILORED TO YOU
             </div>
           </div>
         </div>
 
-        <div className="2xl:mt-[97px]">
-          {/* MODERN URBAN LOFT SECTION */}
+        <div className="2xl:mt-[97px] 2xl:pb-[120px]">
           {modernUrbanData.map((item, index) => (
-            <>
-              <div className=" border-t-1 border-gray-400 ">
-                <div className="2xl:flex items-center justify-between 2xl:py-[22px]">
-                  <div>
-                    <div className="text-[12px] 2xl:w-[109px] 2xl:h-[47px] border-2 border-gray-500 rounded-full flex justify-center items-center">
-                      <div>{item.btnName}</div>
-                    </div>
-                    <div className="2xl:text-[28px] 2xl:mt-[27px] 2xl:font-bold">
-                      {item.heading}
-                    </div>
-                  </div>
+            <div
+              key={index}
+              className="relative  border-t border-gray-400"
+              onMouseEnter={() => setHoveredIndex(index)}
+              onMouseLeave={() => setHoveredIndex(null)}
+            >
+              <div
+                ref={(el) => (overlayRefs.current[index] = el)}
+                className="absolute bottom-0 left-0 w-full h-full bg-gradient-to-r from-[#151800] to-[#596700] opacity-0 translate-y-full z-0"
+              />
 
-                  <div className="2xl:text-[15px] 2xl:leading-[28px] 2xl:w-[504px]">
-                    {item.text}
+              <video
+                ref={(el) => (videoRefs.current[index] = el)}
+                className="absolute top-1/2 left-1/2 transform -translate-x-3/4 -translate-y-1/2 scale-0 opacity-0 z-0 rounded-xl"
+                width="350"
+                height="230"
+                muted
+                playsInline
+                loop
+                src="/playing-game.mp4"
+              />
+
+              <div
+                ref={(el) => (urbanDivRefs.current[index] = el)}
+                className="relative z-10 2xl:flex items-center justify-between 2xl:py-[22px]"
+              >
+                <div>
+                  <div
+                    className={`text-[12px] 2xl:w-[109px] 2xl:h-[47px] border-2 border-gray-500 rounded-full flex justify-center items-center transition duration-300 ${
+                      hoveredIndex === index
+                        ? "bg-white !text-black"
+                        : "bg-transparent text-white"
+                    }`}
+                  >
+                    <div>{item.btnName}</div>
+                  </div>
+                  <div className="2xl:text-[28px] 2xl:mt-[27px] 2xl:font-bold">
+                    {item.heading}
                   </div>
                 </div>
+                <div className="2xl:text-[15px] 2xl:leading-[28px] 2xl:w-[504px]">
+                  {item.text}
+                </div>
               </div>
-            </>
+            </div>
           ))}
         </div>
       </div>
--- src/home/TestimonailSection.jsx
+++ src/home/TestimonailSection.jsx
@@ -1,173 +1,269 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import gsap from "gsap";
 import commentIcon from "../../public/comment-icon.svg";
 import starIcon from "../../public/star-icon.svg";
 import profileAvatar from "../../public/profile-avatar-img.svg";
 import profileAvatar2 from "../../public/profile-avatar-img-2.svg";
 import Image from "next/image";
 
-const TestimonailSection = () => {
-  let testimonialData1 = [
-    {
-      id: 0,
-      name: "Dr. Farrin Manian",
-      company: "Pearl 4 Peers",
-      message:
-        "Even though I am sure mine was a relatively small job for APPGENIE, I felt they took their job just as seriously. Personalized attention and reliability are attributes I appreciate and APPGENIE did not disappoint!",
-      img: profileAvatar,
-      stars: 5,
-    },
+const testimonialData1 = [
+  // truncate: fill with your original data as in the example
+  {
+    id: 0,
+    name: "Dr. Farrin Manian",
+    company: "Pearl 4 Peers",
+    message:
+       "I had asked APPGENIE to create a mobile game application for iOS and Android platforms. Their team was professional throughout the entire mobile application development proc"
+,
+    img: profileAvatar,
+    stars: 5,
+  },
+  {
+    id: 1,
+    name: "Casey Stewart",
+    company: "Switch Poker",
+    message:
+ "Even though I am sure mine was a relatively small job for APPGENIE, I felt they took their job just as seriously. Personalized attention and reliability are attributes I appreciate and APPGENIE did not disappoint!",    img: profileAvatar2,
+    stars: 5,
+  },
+   {
+    id: 0,
+    name: "Dr. Farrin Manian",
+    company: "Pearl 4 Peers",
+    message:
+       "I had asked APPGENIE to create a mobile game application for iOS and Android platforms. Their team was professional throughout the entire mobile application development proc"
+,
+    img: profileAvatar,
+    stars: 5,
+  },
+   {
+    id: 1,
+    name: "Casey Stewart",
+    company: "Switch Poker",
+    message:
+ "Even though I am sure mine was a relatively small job for APPGENIE, I felt they took their job just as seriously. Personalized attention and reliability are attributes I appreciate and APPGENIE did not disappoint!",    img: profileAvatar2,
+    stars: 5,
+  },
+     {
+    id: 0,
+    name: "Dr. Farrin Manian",
+    company: "Pearl 4 Peers",
+    message:
+       "I had asked APPGENIE to create a mobile game application for iOS and Android platforms. Their team was professional throughout the entire mobile application development proc"
+,
+    img: profileAvatar,
+    stars: 5,
+  },
+   {
+    id: 0,
+    name: "Dr. Farrin Manian",
+    company: "Pearl 4 Peers",
+    message:
+       "I had asked APPGENIE to create a mobile game application for iOS and Android platforms. Their team was professional throughout the entire mobile application development proc"
+,
+    img: profileAvatar,
+    stars: 5,
+  },
+  {
+    id: 1,
+    name: "Casey Stewart",
+    company: "Switch Poker",
+    message:
+ "Even though I am sure mine was a relatively small job for APPGENIE, I felt they took their job just as seriously. Personalized attention and reliability are attributes I appreciate and APPGENIE did not disappoint!",    img: profileAvatar2,
+    stars: 5,
+  },
+   {
+    id: 0,
+    name: "Dr. Farrin Manian",
+    company: "Pearl 4 Peers",
+    message:
+       "I had asked APPGENIE to create a mobile game application for iOS and Android platforms. Their team was professional throughout the entire mobile application development proc"
+,
+    img: profileAvatar,
+    stars: 5,
+  },
+   {
+    id: 1,
+    name: "Casey Stewart",
+    company: "Switch Poker",
+    message:
+ "Even though I am sure mine was a relatively small job for APPGENIE, I felt they took their job just as seriously. Personalized attention and reliability are attributes I appreciate and APPGENIE did not disappoint!",    img: profileAvatar2,
+    stars: 5,
+  },
+     {
+    id: 0,
+    name: "Dr. Farrin Manian",
+    company: "Pearl 4 Peers",
+    message:
+       "I had asked APPGENIE to create a mobile game application for iOS and Android platforms. Their team was professional throughout the entire mobile application development proc"
+,
+    img: profileAvatar,
+    stars: 5,
+  },
+   {
+    id: 0,
+    name: "Dr. Farrin Manian",
+    company: "Pearl 4 Peers",
+    message:
+       "I had asked APPGENIE to create a mobile game application for iOS and Android platforms. Their team was professional throughout the entire mobile application development proc"
+,
+    img: profileAvatar,
+    stars: 5,
+  },
+  {
+    id: 1,
+    name: "Casey Stewart",
+    company: "Switch Poker",
+    message:
+ "Even though I am sure mine was a relatively small job for APPGENIE, I felt they took their job just as seriously. Personalized attention and reliability are attributes I appreciate and APPGENIE did not disappoint!",    img: profileAvatar2,
+    stars: 5,
+  },
+   {
+    id: 0,
+    name: "Dr. Farrin Manian",
+    company: "Pearl 4 Peers",
+    message:
+       "I had asked APPGENIE to create a mobile game application for iOS and Android platforms. Their team was professional throughout the entire mobile application development proc"
+,
+    img: profileAvatar,
+    stars: 5,
+  },
+   {
+    id: 1,
+    name: "Casey Stewart",
+    company: "Switch Poker",
+    message:
+ "Even though I am sure mine was a relatively small job for APPGENIE, I felt they took their job just as seriously. Personalized attention and reliability are attributes I appreciate and APPGENIE did not disappoint!",    img: profileAvatar2,
+    stars: 5,
+  },
+     {
+    id: 0,
+    name: "Dr. Farrin Manian",
+    company: "Pearl 4 Peers",
+    message:
+       "I had asked APPGENIE to create a mobile game application for iOS and Android platforms. Their team was professional throughout the entire mobile application development proc"
+,
+    img: profileAvatar,
+    stars: 5,
+  },
+   {
+    id: 0,
+    name: "Dr. Farrin Manian",
+    company: "Pearl 4 Peers",
+    message:
+       "I had asked APPGENIE to create a mobile game application for iOS and Android platforms. Their team was professional throughout the entire mobile application development proc"
+,
+    img: profileAvatar,
+    stars: 5,
+  },
+  {
+    id: 1,
+    name: "Casey Stewart",
+    company: "Switch Poker",
+    message:
+ "Even though I am sure mine was a relatively small job for APPGENIE, I felt they took their job just as seriously. Personalized attention and reliability are attributes I appreciate and APPGENIE did not disappoint!",    img: profileAvatar2,
+    stars: 5,
+  },
+   {
+    id: 0,
+    name: "Dr. Farrin Manian",
+    company: "Pearl 4 Peers",
+    message:
+       "I had asked APPGENIE to create a mobile game application for iOS and Android platforms. Their team was professional throughout the entire mobile application development proc"
+,
+    img: profileAvatar,
+    stars: 5,
+  },
+   {
+    id: 1,
+    name: "Casey Stewart",
+    company: "Switch Poker",
+    message:
+ "Even though I am sure mine was a relatively small job for APPGENIE, I felt they took their job just as seriously. Personalized attention and reliability are attributes I appreciate and APPGENIE did not disappoint!",    img: profileAvatar2,
+    stars: 5,
+  },
+     {
+    id: 0,
+    name: "Dr. Farrin Manian",
+    company: "Pearl 4 Peers",
+    message:
+       "I had asked APPGENIE to create a mobile game application for iOS and Android platforms. Their team was professional throughout the entire mobile application development proc"
+,
+    img: profileAvatar,
+    stars: 5,
+  },
+];
 
-    {
-      id: 1,
-      name: "Casey Stewart",
-      company: "Switch Poker",
-      message:
-        "I had asked APPGENIE to create a mobile game application for iOS and Android platforms. Their team was professional throughout the entire mobile application development proc",
-      img: profileAvatar2,
-      stars: 5,
-    },
+const testimonialData2 = [...testimonialData1]; // or your second list
 
-    {
-      id: 2,
-      name: "Dr. Farrin Manian",
-      company: "Pearl 4 Peers",
-      message:
-        "Even though I am sure mine was a relatively small job for APPGENIE, I felt they took their job just as seriously. Personalized attention and reliability are attributes I appreciate and APPGENIE did not disappoint!",
-      img: profileAvatar,
-      stars: 5,
-    },
-  ];
+const TestimonialCard = ({ data }) => (
+  <div className="bg-white 2xl:mt-[40px] 2xl:w-[574px] 2xl:h-[337px] 2xl:rounded-[20px] 2xl:py-[30px] 2xl:px-[34px]">
+    <div className="2xl:flex justify-between">
+      <Image src={commentIcon} width={40} height={28} alt="comment" />
+      <div className="flex gap-1">
+        {Array.from({ length: data.stars }).map((_, i) => (
+          <Image key={i} src={starIcon} width={26} height={26} alt="star" />
+        ))}
+      </div>
+    </div>
+    <div className="!text-[#414141] 2xl:text-[17px] 2xl:leading-[200%] 2xl:w-[443px] 2xl:pt-[21px]">
+      {data.message}
+    </div>
+    <div className="flex gap-2 2xl:pt-[54px]">
+      <Image src={data.img} width={54} height={54} alt="avatar" />
+      <div>
+        <div className="!text-[#414141] 2xl:text-[16px] font-bold">{data.name}</div>
+        <div className="!text-[#414141]">{data.company}</div>
+      </div>
+    </div>
+  </div>
+);
 
-  let testimonialData2 = [
-    {
-      id: 0,
-      name: "Casey Stewart",
-      company: "Switch Poker",
-      message:
-        "I had asked APPGENIE to create a mobile game application for iOS and Android platforms. Their team was professional throughout the entire mobile application development proc",
-      img: profileAvatar2,
-      stars: 5,
-    },
-    {
-      id: 1,
-      name: "Dr. Farrin Manian",
-      company: "Pearl 4 Peers",
-      message:
-        "Even though I am sure mine was a relatively small job for APPGENIE, I felt they took their job just as seriously. Personalized attention and reliability are attributes I appreciate and APPGENIE did not disappoint!",
-      img: profileAvatar,
-      stars: 5,
-    },
-    {
-      id: 2,
-      name: "Casey Stewart",
-      company: "Switch Poker",
-      message:
-        "I had asked APPGENIE to create a mobile game application for iOS and Android platforms. Their team was professional throughout the entire mobile application development proc",
-      img: profileAvatar2,
-      stars: 5,
-    },
-  ];
+const TestimonialSection = () => {
+  const scrollRef1 = useRef();
+  const scrollRef2 = useRef();
 
-  const renderStars = (count, starIcon) => {
-    return Array.from({ length: count }, (_, index) => (
-      <Image key={index} src={starIcon} width={26} height={26} />
-    ));
-  };
+  useEffect(() => {
+    const el1 = scrollRef1.current;
+    const el2 = scrollRef2.current;
 
-  const testimonialsBgStyle = {
-    backgroundImage: `url("/testimonials-bg.svg")`,
-    // backgroundImage: `url("/crafting-bg-lines.svg")`,
-    backgroundRepeat: "no-repeat",
-    backgroundSize: "cover",
-    backgroundPosition: "center",
-    height: "537px",
-    // backgroundColor: "#D3D3D3",
-    backgroundColor: "#251F1F",
-  };
+    gsap.to(el1, {
+      y: `-${el1.scrollHeight / 2}px`,
+      duration: 60,
+      ease: "none",
+      repeat: -1,
+    });
+
+    gsap.to(el2, {
+      y: `${el2.scrollHeight / 2}px`,
+      duration: 60,
+      ease: "none",
+      repeat: -1,
+    });
+  }, []);
 
   return (
-    <div className="">
-      <div>
-        <div className="2xl:text-center 2xl:text-[64px] !bg-white !text-[#121212] 2xl:py-[38px]">
-          Clients That Trust Our Services
-        </div>
+    <div className="bg-[#121212]">
+      <div className="2xl:text-center 2xl:text-[64px] bg-white !text-[#121212] 2xl:py-[38px]">
+        Clients That Trust Our Services
+      </div>
 
-        {/* <div style={testimonialsBgStyle}>ad</div> */}
-
-        <div className="2xl:flex gap-10 justify-center 2xl:pb-10 bg-[#121212]">
-          <div>
-            {testimonialData1.map((testimonial) => (
-              <div
-                key={testimonial.id}
-                className="!bg-white 2xl:mt-[40px] 2xl:w-[574px] 2xl:h-[337px] 2xl:rounded-[20px] 2xl:py-[30px] 2xl:px-[34px]"
-              >
-                <div>
-                  <div className="2xl:flex justify-between">
-                    <div>
-                      <Image src={commentIcon} width={40} height={28} />
-                    </div>
-                    <div className="flex gap-1">
-                      {renderStars(testimonial.stars, starIcon)}
-                    </div>
-                  </div>
-
-                  <div className="!text-[#414141] 2xl:text-[17px] 2xl:leading-[200%] 2xl:w-[443px] 2xl:pt-[21px]">
-                    {testimonial.message}
-                  </div>
-
-                  <div className="flex gap-2 2xl:pt-[54px]">
-                    <div>
-                      <Image src={testimonial.img} width={54} height={54} />
-                    </div>
-                    <div>
-                      <div className="!text-[#414141] 2xl:text-[16px] font-bold">
-                        {testimonial.name}
-                      </div>
-                      <div className="!text-[#414141]">
-                        {testimonial.company}
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            ))}
+      <div className="2xl:h-[800px] overflow-hidden relative">
+        <div className="2xl:flex justify-center gap-10">
+          <div className="2xl:overflow-hidden h-full">
+            <div ref={scrollRef1}>
+              {[...testimonialData1, ...testimonialData1].map((t, i) => (
+                <TestimonialCard key={i} data={t} />
+              ))}
+            </div>
           </div>
-          <div>
-            {testimonialData2.map((testimonial) => (
-              <div
-                key={testimonial.id}
-                className="!bg-white 2xl:mt-[40px] 2xl:w-[574px] 2xl:h-[337px] 2xl:rounded-[20px] 2xl:py-[30px] 2xl:px-[34px]"
-              >
-                <div>
-                  <div className="2xl:flex justify-between">
-                    <div>
-                      <Image src={commentIcon} width={40} height={28} />
-                    </div>
-                    <div className="flex gap-1">
-                      {renderStars(testimonial.stars, starIcon)}
-                    </div>
-                  </div>
 
-                  <div className="!text-[#414141] 2xl:text-[17px] 2xl:leading-[200%] 2xl:w-[443px] 2xl:pt-[21px]">
-                    {testimonial.message}
-                  </div>
-
-                  <div className="flex gap-2 2xl:pt-[54px]">
-                    <div>
-                      <Image src={testimonial.img} width={54} height={54} />
-                    </div>
-                    <div>
-                      <div className="!text-[#414141] 2xl:text-[16px] font-bold">
-                        {testimonial.name}
-                      </div>
-                      <div className="!text-[#414141]">
-                        {testimonial.company}
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            ))}
+          <div className="2xl:overflow-hidden h-full 2xl:-translate-y-[14000px]">
+            <div ref={scrollRef2}>
+              {[...testimonialData2, ...testimonialData2].map((t, i) => (
+                <TestimonialCard key={i} data={t} />
+              ))}
+            </div>
           </div>
         </div>
       </div>
@@ -175,4 +271,4 @@ const TestimonailSection = () => {
   );
 };
 
-export default TestimonailSection;
+export default TestimonialSection;
